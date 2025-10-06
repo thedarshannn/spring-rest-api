1e11bdf892af876411f6559818ffb0da66bbd2c2
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2025-10-06 13:32:48 -0400

    Update SoftwareEngineerController to return a predefined list of software engineers

--- src/main/java/dev/darshan/controllers/SoftwareEngineerController.java
+++ src/main/java/dev/darshan/controllers/SoftwareEngineerController.java
@@ -12,19 +12,14 @@ import java.util.List;
 public class SoftwareEngineerController {
 
 
+    SoftwareEngineer s1 = new SoftwareEngineer(1, "Darshan", List.of("Java", "Spring Boot", "Python"));
+    SoftwareEngineer s2 = new SoftwareEngineer(2, "Alice", List.of("JavaScript", "React", "Node.js"));
+    SoftwareEngineer s3 = new SoftwareEngineer(3, "Bob", List.of("C#", ".NET", "Azure"));
+
+
+
     @GetMapping
     public List<SoftwareEngineer> getSoftwareEngineers() {
-            return List.of(
-                    new SoftwareEngineer(
-                            1,
-                            "James",
-                            "js,java"
-                    ),
-                    new SoftwareEngineer(
-                            2,
-                            "Julia",
-                            "js,react"
-                    )
-            );
+        return List.of(s1, s2, s3);
     }
 }
