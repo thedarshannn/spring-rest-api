066d7c3db788b9bb63448f70d40175dfd0ef4ae6
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2025-10-06 10:50:16 -0400

    Add initial HTTP request file and implement a simple REST endpoint

--- src/main/java/dev/darshan/SpringRestApiApplication.java
+++ src/main/java/dev/darshan/SpringRestApiApplication.java
@@ -2,12 +2,20 @@ package dev.darshan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringRestApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringRestApiApplication.class, args);
+    }
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World from my first Spring Boot!";
     }
 
 }
