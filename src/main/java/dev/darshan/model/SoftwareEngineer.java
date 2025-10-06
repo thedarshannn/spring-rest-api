5cfc3fe20f0c371c5d207cb09c964d463b158e8c
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2025-10-06 12:15:52 -0400

    Add SoftwareEngineer model class with properties and methods

---
+++ src/main/java/dev/darshan/model/SoftwareEngineer.java
@@ -0,0 +1,59 @@
+package dev.darshan.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class SoftwareEngineer {
+
+    private Integer id;
+    private String name;
+    private List<String> techStack;
+
+
+    public SoftwareEngineer() {
+    }
+
+    public SoftwareEngineer(Integer id,
+                            String name,
+                            List<String> techStack) {
+        this.id = id;
+        this.name = name;
+        this.techStack = techStack;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getTechStack() {
+        return techStack;
+    }
+
+    public void setTechStack(List<String> techStack) {
+        this.techStack = techStack;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SoftwareEngineer that = (SoftwareEngineer) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(techStack, that.techStack);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, techStack);
+    }
+}
